2f4b97159ed8f41948468f5ea2f383adfe14e596
Dmytro Ruschak <rus4ak331@gmail.com>
2023-06-02 19:42:36 +0200

    changet the database to sqlite

--- MVP/MVP/settings.py
+++ MVP/MVP/settings.py
@@ -95,12 +95,8 @@ WSGI_APPLICATION = 'MVP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NAME'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': env('PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
