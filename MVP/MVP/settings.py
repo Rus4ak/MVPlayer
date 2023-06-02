b1bbf49f5ab1e948f6a125f923655d2a434b4ada
Dmytro Ruschak <rus4ak331@gmail.com>
2023-06-02 16:56:10 +0200

    changed the database to postgresql and fixed the bug in css

--- MVP/MVP/settings.py
+++ MVP/MVP/settings.py
@@ -95,10 +95,15 @@ WSGI_APPLICATION = 'MVP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
+
 
 
 
