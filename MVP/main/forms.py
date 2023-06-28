1ab343d50bcdb380bc16b97f8c00534f07412d32
Dmytro Ruschak <rus4ak331@gmail.com>
2023-06-28 09:51:45 +0200

    fixed bug

--- MVP/main/forms.py
+++ MVP/main/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from .models import Song, Playlist
+from .models import Song, Playlist, Genre
 
 class AudioFileForm(forms.ModelForm):
     title = forms.CharField(label=_('Название'))
     icon = forms.ImageField(label=_('Иконка'))
     audio_file = forms.FileField(label=_('Аудио файл'))
-    genre = forms.CharField(label=_('Жанр'))
+    genre = forms.ModelMultipleChoiceField(
+        queryset=Genre.objects.all(),
+        label=_('Жанр'),
+        widget=forms.SelectMultiple
+    )
 
     class Meta:
         model = Song
